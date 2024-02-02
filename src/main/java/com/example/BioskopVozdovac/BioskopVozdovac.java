1b9f631ab2479cfc3dae16c10a057bc349661f18
Konevozd <nemanja543210@gmail.com>
2024-02-02 18:56:26 +0100

    Initialization

---
+++ src/main/java/com/example/BioskopVozdovac/BioskopVozdovac.java
@@ -0,0 +1,13 @@
+package com.example.BioskopVozdovac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioskopVozdovac {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BioskopVozdovac.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BioskopVozdovac/BioskopVozdovacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BioskopVozdovac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioskopVozdovacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
