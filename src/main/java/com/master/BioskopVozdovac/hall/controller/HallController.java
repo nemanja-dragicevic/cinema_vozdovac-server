be64c9d8cdb6ae1c05d1d285de3ccff9848fc8e8
nemanja-dragicevic <nd201900065@student.fon.bg.ac.rs>
2024-04-18 17:02:48 +0200

    Role-based security for hall and member APIs

--- src/main/java/com/master/BioskopVozdovac/hall/controller/HallController.java
+++ src/main/java/com/master/BioskopVozdovac/hall/controller/HallController.java
@@ -5,6 +5,7 @@ import com.master.BioskopVozdovac.hall.service.HallService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,11 +23,13 @@ public class HallController {
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<HallDTO> createHall(@RequestBody HallDTO dto) {
         return new ResponseEntity<>(hallService.createHall(dto), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<String> deleteHall(@PathVariable Long id) {
         return new ResponseEntity<>(hallService.deleteWithId(id), HttpStatus.OK);
     }
