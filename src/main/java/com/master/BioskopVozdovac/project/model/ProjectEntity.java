253eb1625e87e3616897e0c67bc53d1b0f03c0b6
nemanja-dragicevic <nd201900065@student.fon.bg.ac.rs>
2024-07-07 12:43:29 +0200

    Documented ProjectEntity, RoleEntity and SeatEntity

--- src/main/java/com/master/BioskopVozdovac/project/model/ProjectEntity.java
+++ src/main/java/com/master/BioskopVozdovac/project/model/ProjectEntity.java
@@ -14,6 +14,11 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Represents a project entity in the application.
+ *
+ * @author Nemanja Dragićević
+ */
 @Entity
 @Getter
 @Setter
@@ -22,30 +27,54 @@ import java.util.Set;
 @Table(name = "projects")
 public class ProjectEntity {
 
+    /**
+     * Unique identifier for the projection.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "project_id", nullable = false)
     private Long projectID;
 
+    /**
+     * The movie associated with this projection.
+     * Represents the many-to-one relationship between ProjectEntity and MovieEntity.
+     */
     @ManyToOne
     @NotNull
     @JoinColumn(name = "movie_id", referencedColumnName = "id")
     private MovieEntity movie;
 
+    /**
+     * The hall associated with this project.
+     * Represents the many-to-one relationship between ProjectEntity and HallEntity.
+     */
     @ManyToOne
     @NotNull
     @JoinColumn(name = "hall_id", referencedColumnName = "id")
     private HallEntity hall;
 
+    /**
+     * The start time of the projection.
+     */
     @Column(name = "starts_at")
     private LocalDateTime time;
 
+    /**
+     * The end time of the projection.
+     */
     @Column(name = "ends_at")
     private LocalDateTime projectEnd;
 
+    /**
+     * The price of the projection.
+     */
     @Column(name = "price")
     private Long price;
 
+    /**
+     * Set of ticket items associated with this project.
+     * Represents the one-to-many relationship between ProjectEntity and TicketItemEntity.
+     */
     @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
     private Set<TicketItemEntity> ticketItems = new HashSet<>();
 
--- src/main/java/com/master/BioskopVozdovac/seat/model/SeatEntity.java
+++ src/main/java/com/master/BioskopVozdovac/seat/model/SeatEntity.java
@@ -9,6 +9,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Represents a seat entity in the application.
+ *
+ * @author Nemanja Dragićević
+ */
 @Getter
 @Setter
 @AllArgsConstructor
@@ -17,23 +22,40 @@ import lombok.Setter;
 @Table(name = "seat")
 public class SeatEntity {
 
+    /**
+     * Unique identifier for the seat.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "seat_id", nullable = false)
     private Long id;
 
+    /**
+     * The row number of the seat.
+     */
     @NotNull
     @Column(name = "row_number")
     private int rowNumber;
 
+    /**
+     * The seat number within the row.
+     */
     @NotNull
     @Column(name = "seat_number")
     private int seatNumber;
 
+    /**
+     * The hall to which this seat belongs.
+     * Represents the many-to-one relationship between SeatEntity and HallEntity.
+     */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "hall_id")
     private HallEntity hall;
 
+    /**
+     * The ticket item associated with this seat.
+     * Represents the one-to-one relationship between SeatEntity and TicketItemEntity.
+     */
     @OneToOne(mappedBy = "seat")
     private TicketItemEntity ticketItem;
 
