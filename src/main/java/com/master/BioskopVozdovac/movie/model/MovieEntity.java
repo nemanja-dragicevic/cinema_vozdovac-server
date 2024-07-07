7ea498423338ffafb81f819a4fe7bb296045f421
nemanja-dragicevic <nd201900065@student.fon.bg.ac.rs>
2024-07-07 12:36:33 +0200

    Documented MovieEntity

--- src/main/java/com/master/BioskopVozdovac/movie/model/MovieEntity.java
+++ src/main/java/com/master/BioskopVozdovac/movie/model/MovieEntity.java
@@ -12,6 +12,11 @@ import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Represents a movie entity in the application.
+ *
+ * @author Nemanja Dragićević
+ */
 @Getter
 @Setter
 @ToString
@@ -21,30 +26,60 @@ import java.util.Set;
 @Table(name = "movie")
 public class MovieEntity {
 
+    /**
+     * Unique identifier for the movie.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long movieID;
 
+    /**
+     * The name of the movie.
+     */
     @NotEmpty
     private String name;
 
+    /**
+     * The detailed description of the movie.
+     */
     @NotEmpty
     private String description;
 
+    /**
+     * The duration of the movie in minutes.
+     */
     @Positive
     private int duration;
 
+    /**
+     * The start time of the movie.
+     */
     private LocalDate startTime;
 
+    /**
+     * The end time of the movie.
+     */
     private LocalDate endTime;
 
+    /**
+     * Set of roles associated with this movie.
+     * Represents the one-to-many relationship between MovieEntity and RoleEntity.
+     */
     @OneToMany(mappedBy = "movie", cascade = CascadeType.ALL)
     private Set<RoleEntity> roles = new HashSet<>();
 
+    /**
+     * Set of projects associated with this movie.
+     * Represents the one-to-many relationship between MovieEntity and ProjectEntity.
+     */
     @OneToMany(mappedBy = "movie", cascade = CascadeType.ALL)
     private Set<ProjectEntity> projects = new HashSet<>();
 
+    /**
+     * Set of genres associated with this movie.
+     * Represents the many-to-many relationship between MovieEntity and GenreEntity.
+     */
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
             name = "movie_genres",
