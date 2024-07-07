efe3ff4baa5666d5ff20008df06bec491ba25c3b
nemanja-dragicevic <nd201900065@student.fon.bg.ac.rs>
2024-07-07 13:04:40 +0200

    Documented TicketEntity and TicketItemEntity and updated TicketStatus

--- src/main/java/com/master/BioskopVozdovac/ticket/model/TicketItemEntity.java
+++ src/main/java/com/master/BioskopVozdovac/ticket/model/TicketItemEntity.java
@@ -8,6 +8,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Represents a ticket item entity in the application.
+ *
+ * @author Nemanja Dragićević
+ */
 @Getter
 @Setter
 @AllArgsConstructor
@@ -16,23 +21,34 @@ import lombok.Setter;
 @Table(name = "ticket_items")
 public class TicketItemEntity {
 
+    /**
+     * Unique identifier for the ticket item.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "item_id", nullable = false)
     private Long id;
 
+    /**
+     * The ticket associated with this ticket item.
+     * Represents the many-to-one relationship between TicketItemEntity and TicketEntity.
+     */
     @ManyToOne
     @JoinColumn(name = "ticket_id")
     private TicketEntity ticket;
-//
-//    @NotEmpty
-//    @Column(name = "number_of_seats")
-//    private int numberOfSeats;
 
+    /**
+     * The projection associated with this ticket item.
+     * Represents the many-to-one relationship between TicketItemEntity and ProjectEntity.
+     */
     @ManyToOne
     @JoinColumn(name = "project_id")
     private ProjectEntity project;
 
+    /**
+     * The seat associated with this ticket item.
+     * Represents the one-to-one relationship between TicketItemEntity and SeatEntity.
+     */
     @OneToOne
     @JoinColumn(name = "seat_id")
     private SeatEntity seat;
