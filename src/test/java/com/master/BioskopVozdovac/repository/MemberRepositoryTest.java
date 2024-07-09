dcd4492d9a3a571620f4cabc1e932fdcdc19ad64
nemanja-dragicevic <nd201900065@student.fon.bg.ac.rs>
2024-07-09 17:05:57 +0200

    Moved tested classes

--- src/test/java/com/master/BioskopVozdovac/repository/MemberRepositoryTest.java
+++ src/test/java/com/master/BioskopVozdovac/repository/MemberRepositoryTest.java
@@ -6,41 +6,28 @@
 //import org.junit.jupiter.api.Assertions;
 //import org.junit.jupiter.api.Test;
 //import org.junit.jupiter.api.extension.ExtendWith;
+//import org.junit.runner.RunWith;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 //import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import org.springframework.test.context.junit4.SpringRunner;
 //
 //import static com.master.BioskopVozdovac.input.MemberData.*;
 //import static org.junit.jupiter.api.Assertions.assertEquals;
 //
 ////@ExtendWith(SpringExtension.class)
+//@RunWith(SpringRunner.class)
 //@DataJpaTest
-//public class MemberRepositoryTest implements CRUDRepositoryTest {
+//public class MemberRepositoryTest {
 //
 //    @Autowired
 //    private MemberRepository memberRepository;
 //
-//    @Override
 //    @Test
 //    public void testSave() {
 //        MemberEntity entity = memberRepository.save(MEMBER_ENTITY);
 //
 //        assertEquals(MEMBER_ENTITY.getFirstName(), entity.getFirstName());
-//    }
-//
-//    @Override
-//    public void testUpdate() {
-//
-//    }
-//
-//    @Override
-//    public void testFind() {
-//
-//    }
-//
-//    @Override
-//    public void testDelete() {
-//
 //    }
 //
 //}
