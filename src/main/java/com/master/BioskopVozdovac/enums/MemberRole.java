a5159337f73d0ce1b67b8544b60c178d4d2bdde4
nemanja-dragicevic <nd201900065@student.fon.bg.ac.rs>
2024-07-10 18:01:25 +0200

    Documented MemberRole and User classes

--- src/main/java/com/master/BioskopVozdovac/enums/MemberRole.java
+++ src/main/java/com/master/BioskopVozdovac/enums/MemberRole.java
@@ -2,10 +2,29 @@ package com.master.BioskopVozdovac.enums;
 
 import org.springframework.security.core.GrantedAuthority;
 
+/**
+ * Enum representing roles of members in the system.
+ * Implements GrantedAuthority to integrate with Spring Security's role-based authorization.
+ *
+ * @author Nemanja Dragićević
+ */
 public enum MemberRole implements GrantedAuthority {
 
-    ADMIN, USER;
+    /**
+     * Administrator role.
+     */
+    ADMIN,
 
+    /**
+     * Regular user role.
+     */
+    USER;
+
+    /**
+     * Returns the authority (role name) as a string.
+     *
+     * @return The name of the enum constant representing the authority (role name).
+     */
     @Override
     public String getAuthority() {
         return this.name();
