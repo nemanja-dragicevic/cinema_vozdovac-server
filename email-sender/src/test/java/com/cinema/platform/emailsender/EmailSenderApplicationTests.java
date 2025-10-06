a9f0ef27f86a420ee2c89ef1d3c20a4bd9c51191
nemanja-dragicevic <nemanja543210@gmail.com>
2025-10-06 13:03:02 +0200

    removed context load for email sender

--- email-sender/src/test/java/com/cinema/platform/emailsender/EmailSenderApplicationTests.java
+++ email-sender/src/test/java/com/cinema/platform/emailsender/EmailSenderApplicationTests.java
@@ -1,13 +1,12 @@
 package com.cinema.platform.emailsender;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EmailSenderApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
